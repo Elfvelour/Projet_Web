b89a019cf8d494e86f238da0d0de9417364aac8f
Elfvelour <timothee.girault@efrei.net>
2026-05-07 08:16:32 +0200

    bug google done

--- js/script.js
+++ js/script.js
@@ -138,50 +138,6 @@ buttons.forEach((button, index) => {
     });
 });
 
-/*-----------------------Graphique-----------------------------*/ 
-google.charts.load("current", {packages:['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ["Note", "Satisfaction (en %)", { role: "style" }],
-        ["Très bien", 80.78, "green"],
-        ["Bien", 10.31, "#7fff00"],
-        ["Moyen", 6.67, "gray"],
-        ["Mécontent", 2.24, "red"]
-    ]);
-
-    var view = new google.visualization.DataView(data);
-    view.setColumns([0, 1,
-        { calc: "stringify", sourceColumn: 1, type: "string", role: "annotation" },
-        2]);
-
- 
-   var chartDiv = document.getElementById("chart_div");
-   var chartWidth = chartDiv.offsetWidth || 600;
-   var chartHeight;
-   if(window.innerWidth < 600){
-      chartHeight = 200;
-   } 
-   else if(window.innerWidth < 768){
-      chartHeight = 250;
-   }
-   else {
-      chartHeight = 400;
-   }
-
-   var options = {
-      title: "Satisfaction étudiante sur le département informatique",
-      width: chartWidth,
-      height: chartHeight,
-      bar: {groupWidth: "95%"},
-      legend: { position: "none" },
-   };
-
-    var chart = new google.visualization.ColumnChart(chartDiv);
-    chart.draw(view, options);
-}
-window.addEventListener('resize', drawChart);
 
 /*-------------------FAQ et plan d'accès-----------------------*/
 function triangle_FAQ(id, event) {
@@ -271,9 +227,6 @@ async function chargerFAQ() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    chargerFAQ();
-});
 
 /*-------------------Plan d'accès-----------------------*/
 function switchCampus(campusId, btn) {
@@ -295,6 +248,9 @@ function switchCampus(campusId, btn) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    chargerFAQ();
+
     const textes = document.querySelectorAll('.timeline-content');
 
     textes.forEach(texte => {
@@ -306,6 +262,9 @@ document.addEventListener('DOMContentLoaded', () => {
             texte.closest('.container2').classList.remove('active');
         });
     });
+
+    responsive_actu();
+
 });
 
 /*-----------------Actualités dans la page d'accueil (responsive)------------------*/
@@ -323,7 +282,6 @@ function responsive_actu() {
 }
 
 window.addEventListener('resize', responsive_actu);
-document.addEventListener('DOMContentLoaded', responsive_actu);
 
 /*-----------------Formulaire Enseignants_Recherche------------------*/
 function envoyerFormulaire() {
