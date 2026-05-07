35842424e98df60fc6c84d651b77d0d92b89397e
Elfvelour <timothee.girault@efrei.net>
2026-05-07 09:18:22 +0200

    questionnaire reset

--- js/script.js
+++ js/script.js
@@ -285,13 +285,17 @@ window.addEventListener('resize', responsive_actu);
 
 /*-----------------Formulaire Enseignants_Recherche------------------*/
 function envoyerFormulaire() {
-            const nom = document.getElementById('nom').value.trim();
-            const email = document.getElementById('email').value.trim();
-            const sujet = document.getElementById('sujet').value.trim();
-            const message = document.getElementById('message').value.trim();
-            if (!nom || !email || !sujet || !message) {
-                alert('Veuillez remplir tous les champs obligatoires (*).');
-                return;
-            }
-            document.getElementById('form-confirmation').style.display = 'block';
-        }
+    const nom = document.getElementById('nom').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const sujet = document.getElementById('sujet').value.trim();
+    const message = document.getElementById('message').value.trim();
+    if (!nom || !email || !sujet || !message) {
+        alert('Veuillez remplir tous les champs obligatoires (*).');
+        return;
+    }
+    document.getElementById('form-confirmation').style.display = 'block';
+    setTimeout(() => {
+        document.getElementById('contact-form').reset();        
+        document.getElementById('form-confirmation').style.display = 'none'; 
+    }, 2000);
+}
